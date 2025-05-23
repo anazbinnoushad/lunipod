f886b6d36f190e91eb9b8aec9f2b7db3c2dba515
Anaz N <anazbinnoushad@gmail.com>
2025-05-23 18:21:51 +0530

    Add tailwind v4

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
+import Image, {type ImageProps} from "next/image";
+import {Button} from "@repo/ui/button";
 import styles from "./page.module.css";
 
 type Props = Omit<ImageProps, "src"> & {
@@ -8,7 +8,7 @@ type Props = Omit<ImageProps, "src"> & {
 };
 
 const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
+  const {srcLight, srcDark, ...rest} = props;
 
   return (
     <>
