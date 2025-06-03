b35ef5ad39e4864c8c9ef20c27d17251ec99cf70
Anaz N <anazbinnoushad@gmail.com>
2025-06-03 23:09:25 +0530

    Update sidebar

--- apps/docs/src/components/sidebar/Sidebar.tsx
+++ apps/docs/src/components/sidebar/Sidebar.tsx
@@ -1,19 +1,29 @@
+"use client";
+
 import Link from "next/link";
 import {nav} from "./nav";
+import {usePathname} from "next/navigation";
+import {Badge} from "../ui/badge";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <nav className="pl-5 flex flex-col gap-2">
-      {nav.map(({title, header, route}, idx) => {
-        const key = `${title}_${idx}`;
-        return header ? (
+      {nav.map((navItem, idx) => {
+        const key = `${navItem.title}_${idx}`;
+        const href = `/components/${navItem.route}`;
+        const isActive = pathname === href;
+        return navItem.header ? (
           <h3 className="text-lg mb-1" key={key}>
-            {title}
+            {navItem.title}
           </h3>
         ) : (
-          <Link href={`/components/${route!}`} key={key}>
-            <h4 className="text-base font-light text-muted-foreground">
-              {title}
+          <Link href={href} key={key}>
+            <h4
+              className={`text-base flex gap-2 items-center font-light text-muted-foreground hover:translate-x-1 hover:text-white transition-all transform-cpu ${isActive && ` text-white`}`}
+            >
+              {navItem.title}
+              {navItem.badge && <Badge>{navItem.badge}</Badge>}
             </h4>
           </Link>
         );
--- apps/docs/src/components/sidebar/nav.ts
+++ apps/docs/src/components/sidebar/nav.ts
@@ -1,11 +1,11 @@
 export type NavItem =
   | {title: string; header: true; route?: never}
-  | {title: string; route: string; header?: never};
+  | {title: string; route: string; header?: never; badge?: string};
 
 export const nav: NavItem[] = [
   {title: "Components", header: true},
   {title: "Testing", route: "test-face"},
   {title: "Split Text", route: "split-text"},
-  {title: "Unfolding Text", route: "unfolding-text"},
+  {title: "Unfolding Text", route: "unfolding-text", badge: "New"},
   {title: "Magnetic Text", route: "magnetic-text"},
 ];
